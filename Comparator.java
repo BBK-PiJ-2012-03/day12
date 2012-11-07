6037d72f9d95f5b5afdcf2853ceb2160763a1d16
Alex <a.rokotas@gmail.com>
2012-11-07 14:20:10 +0000

    Now it returns the type of the method called. (More precise)

--- Comparator.java
+++ Comparator.java
@@ -1,14 +1,15 @@
 public class Comparator {
-	public double getMax(int n, int m) {
-	double d1 = (double) n;
-	double d2 = (double) m;
-	return(getMax(d1,d2));
+	public int getMax(int n, int m) {
+		double d1 = (double) n;
+		double d2 = (double) m;
+		return (int)(getMax(d1,d2));
 	}
 	
-	public double getMax(String number1, String number2) {
-	double d1 = Double.parseDouble(number1);
-	double d2 = Double.parseDouble(number2);
-	return(getMax(d1,d2));
+	public String getMax(String number1, String number2) {
+		double d1 = Double.parseDouble(number1);
+		double d2 = Double.parseDouble(number2);
+		String result = Double.toString(getMax(d1,d2));
+		return result;
 	}
 	
 	public double getMax(double d1, double d2) {
--- ComparatorScript.java
+++ ComparatorScript.java
@@ -7,8 +7,8 @@ public class ComparatorScript {
  
 	public void launch() {
 		Comparator myComp = new Comparator();
-		System.out.println(myComp.getMax(1,2));
-		System.out.println(myComp.getMax(1.0,2.0));
-		System.out.println(myComp.getMax("1","2"));
+		System.out.println("Calling the int getMax method to compare 1 and 2, the result is " +myComp.getMax(1,2));
+		System.out.println("Calling the double getMax method to compare 1 and 2, the result is " + myComp.getMax(1.0,2.0));
+		System.out.println("Calling the String getMax method to compare 1 and 2, the result is " + myComp.getMax("1","2"));
 	}	
 }	
